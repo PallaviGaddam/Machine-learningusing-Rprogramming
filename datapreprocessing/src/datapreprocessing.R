86f41298c30cd54cdf7dbdd592d4d926727c9b0d
PallaviGaddam <b9058141@newcastle.ac.uk>
2020-04-09 21:41:08 +0100

    Processing data

---
+++ datapreprocessing/src/datapreprocessing.R
@@ -0,0 +1,40 @@
+#data preprocessing
+library(dplyr)
+library(plyr)
+
+#importing the dataset
+
+
+data <- read.csv("~/Machine-learningusing-Rprogramming/datapreprocessing/data/Data.csv")
+
+
+#missing data
+data$Age<-ifelse(is.na(data$Age),ave(data$Age,FUN = function(x) mean(x, na.rm = TRUE)), data$Age)
+data$Salary= ifelse(is.na(data$Salary),
+                    ave(data$Salary,FUN = function(x) mean(x, na.rm = TRUE)),
+                    data$Salary)
+
+#encoding categorical data
+# as country and purchased are categorical variables we need to convert them
+
+data$Country=factor(data$Country,
+                    levels = c('France','Spain','Germany'),
+                    labels = c(1,2,3))
+
+data$Purchased=factor(data$Purchased,
+                      levels = c('No','Yes'),
+                      labels = c(0,1))
+
+##splitting the data into train and test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split=sample.split(data$Purchased,SplitRatio = 0.8)
+training_set=subset(data,split==TRUE)
+test_set=subset(data,split==FALSE)
+
+
+#scaling the train and test data sets
+training_set[,2:3]=scale(training_set[,2:3])
+test_set[,2:3]=scale(test_set[,2:3])
+
